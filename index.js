10c6e93fb10bc8ce08ffff482bea85885f7b8543
shahid1105 <shahidhasan1105@gmail.com>
2023-06-08 17:52:38 +0600

    my selected card delete operation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.08jlhdc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -65,6 +65,15 @@ async function run() {
             const item = req.body;
 
             const result = await selectedClassCollection.insertOne(item);
+            res.send(result)
+        })
+
+
+        app.delete('/class-carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const result = await selectedClassCollection.deleteOne(query);
             res.send(result)
         })
 
